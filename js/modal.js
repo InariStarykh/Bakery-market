fdea96876ccd66bcbd90cc41f7eb783c7d541283
InariStarykh <innastarykh2025@gmail.com>
2025-06-12 15:07:08 +0300

    Add js

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,13 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnOpenList = document.querySelectorAll('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+const toggleModal = () => {
+    modal.classList.toggle('is-hidden');
+};
 
-modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnOpenList.forEach((btn) => {
+  btn.addEventListener('click', toggleModal);
+});
+
 modalBtnClose.addEventListener('click', toggleModal);
